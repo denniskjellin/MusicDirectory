b3aad5c38c7f14e3db3d23870ef40dc5dd1661db
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-08 15:15:20 +0100

    initiating mVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MusicDirectory.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
